fe15e7bff4b1de8cc2c1a5ec76cc08a57538c827
hemanthboinpally <hemanth.boinpally@gmail.com>
2019-05-18 16:12:15 -0700

    Remove low variance columns

--- feature_selection_1.py
+++ feature_selection_1.py
@@ -74,3 +74,54 @@ when we feed it too many features. It overlooks that neck circumference by itsel
 and females.
 
 """
+
+#####################
+
+
+"""
+3) Finding a good variance threshold
+
+You'll be working on a slightly modified subsample of the ANSUR dataset with just head measurements 
+pre-loaded as head_df.
+
+boxplot, .boxplot()
+
+normalize, df/ df.mean()
+
+check variance of columns, .var()
+
+remove the columns with the lowest variance 
+
+"""
+
+# Create the boxplot
+head_df.boxplot()
+
+plt.show()
+
+
+# Normalize the data
+normalized_df = head_df / head_df.mean()
+
+normalized_df.boxplot()
+plt.show()
+
+
+# Normalize the data
+normalized_df = head_df / head_df.mean()
+
+# Print the variances of the normalized data
+print(normalized_df.var())
+
+
+"""
+
+Here the threshold is 1.0e-03 to two lowest variance columns.
+
+headbreadth          1.678952e-03
+headcircumference    1.029623e-03
+headlength           1.867872e-03
+tragiontopofhead     2.639840e-03
+n_hairs              1.002552e-08
+measurement_error    3.231707e-27
+"""
