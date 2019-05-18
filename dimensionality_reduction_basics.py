4750e62653cab0a0978d9125a7d293b1e6db417e
hemanthboinpally <hemanth.boinpally@gmail.com>
2019-05-18 15:19:38 -0700

    SVC model check overfit

--- dimensionality_reduction_basics.py
+++ dimensionality_reduction_basics.py
@@ -8,7 +8,7 @@ shape (row,columns)
 """
 
 """
-Removing features without variance. 
+1) Removing features without variance. 
 
 """
 
@@ -63,7 +63,7 @@ Here Legendary has no variance, it has the same values. So we can remove the val
 ###########
 
 """
-Feature Selection Vs Feature Extraction:
+2) Feature Selection Vs Feature Extraction:
 
 Visually detecting redundant features
 Data visualization is a crucial step in any data exploration. Let's use Seaborn to explore some samples of the US Army
@@ -104,7 +104,7 @@ plt.show()
 ###########################
 
 """
-t-SNE visualization of high-dimensional data
+3) t-SNE visualization of high-dimensional data
 
 Fitting t-SNE to the ANSUR data
 t-SNE is a great technique for visual exploration of high dimensional datasets. In this exercise, you'll apply it to 
@@ -140,5 +140,43 @@ OUTPUT:
  
 t-SNE reduced the more than 90 features in the dataset to just 2 which we can now plot.
 
+"""
+
+
+#########################
+
+"""
+4)  t-SNE visualisation of dimensionality
+Time to look at the results of your hard work. In this exercise, you will visualize the output of t-SNE dimensionality 
+reduction on the combined male and female Ansur dataset. You'll create 3 scatterplots of the 2 t-SNE features ('x' and 'y') which were added to the dataset df. In each scatterplot you'll color the points according to a different categorical variable.
+
+seaborn has already been imported as sns and matplotlib.pyplot as plt.
+
+"""
+# Color the points according to Army Component
+sns.scatterplot(x="x", y="y", hue='Component', data=df)
+
+# Show the plot
+plt.show()
+
+# Color the points by Army Branch
+sns.scatterplot(x="x", y="y", hue='Branch', data=df)
+
+# Show the plot
+plt.show()
+
+# Color the points by Gender
+sns.scatterplot(x="x", y="y", hue='Gender', data=df)
+
+# Show the plot
+plt.show()
+
+
+
+"""
+Eureka! There is a Male and a Female cluster. t-SNE found these gender differences in body shape without being told 
+about them explicitly! From the second plot you learned there are more males in the Combat Arms Branch.
+
+Refer Notes for Plots:
 """
 
--- feature_selection_1.py
+++ feature_selection_1.py
@@ -1,7 +1,7 @@
 #############
 
 """
-Testing you model for Overfitting
+1) Testing you model for Overfitting
 
 """
 
@@ -20,3 +20,57 @@ accuracy_train = accuracy_score(y_train, svc.predict(X_train))
 accuracy_test = accuracy_score(y_test, svc.predict(X_test))
 
 print("{0:.1%} accuracy on test set vs. {1:.1%} on training set".format(accuracy_test, accuracy_train))
+
+
+"""
+OUTPUT:
+49.7% accuracy on test set vs. 100.0% on training set
+
+Looks like the model is badly overfit on the training data. On unseen data it performs worse than a random selector 
+would.
+
+"""
+
+
+###############
+
+"""
+
+2) Accuracy after dimensionality reduction - Can reduce the overfit after the dimensionality reduction
+
+You'll reduce the overfit with the help of dimensionality reduction. In this case, you'll apply a rather drastic from 
+of dimensionality reduction by only selecting a single column that has some good information to distinguish between genders. 
+You'll repeat the train-test split, model fit and prediction steps to compare the accuracy on test vs. training data.
+
+All relevant packages and y have been pre-loaded.
+
+
+
+"""
+
+
+# Assign just the 'neckcircumferencebase' column from ansur_df to X
+X = ansur_df[['neckcircumferencebase']]
+
+# Split the data, instantiate a classifier and fit the data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+svc = SVC()
+svc.fit(X_train, y_train)
+
+# Calculate accuracy scores on both train and test data
+accuracy_train = accuracy_score(y_train, svc.predict(X_train))
+accuracy_test = accuracy_score(y_test, svc.predict(X_test))
+
+print("{0:.1%} accuracy on test set vs. {1:.1%} on training set".format(accuracy_test, accuracy_train))
+
+
+"""
+   OUTPUT:
+    93.3% accuracy on test set vs. 94.9% on training set
+
+Wow, what just happened!? On the full dataset the model is rubbish but with a single feature we can make 
+good predictions? This is an example of the curse of dimensionality! The model badly overfits 
+when we feed it too many features. It overlooks that neck circumference by itself is pretty different for males 
+and females.
+
+"""
