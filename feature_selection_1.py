3fb9d501041bb25310a7375177e3d44a9aa67854
hemanthboinpally <hemanth.boinpally@gmail.com>
2019-05-18 15:12:02 -0700

    SVC model check overfit

---
+++ feature_selection_1.py
@@ -0,0 +1,22 @@
+#############
+
+"""
+Testing you model for Overfitting
+
+"""
+
+# Import SVC from sklearn.svm and accuracy_score from sklearn.metrics
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+# Create an instance of the Support Vector Classification class
+svc = SVC()
+
+# Fit the model to the training data
+svc.fit(X_train, y_train)
+
+# Calculate accuracy scores on both train and test data
+accuracy_train = accuracy_score(y_train, svc.predict(X_train))
+accuracy_test = accuracy_score(y_test, svc.predict(X_test))
+
+print("{0:.1%} accuracy on test set vs. {1:.1%} on training set".format(accuracy_test, accuracy_train))
