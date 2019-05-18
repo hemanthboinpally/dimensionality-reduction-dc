e90120991525895282f00674558cc5ff7a71a777
hemanthboinpally <hemanth.boinpally@gmail.com>
2019-05-18 16:16:41 -0700

    Remove low variance columns- VarianceThreshold

--- feature_selection_1.py
+++ feature_selection_1.py
@@ -125,3 +125,38 @@ tragiontopofhead     2.639840e-03
 n_hairs              1.002552e-08
 measurement_error    3.231707e-27
 """
+
+
+#######################
+
+"""
+4) Features with low variance
+In the previous exercise you established that 0.001 is a good threshold to filter out low variance 
+features in head_df after normalization. Now use the VarianceThreshold feature selector to remove these features.
+
+"""
+from sklearn.feature_selection import VarianceThreshold
+
+# Create a VarianceThreshold feature selector
+sel = VarianceThreshold(threshold=0.001)
+
+# Fit the selector to normalized head_df
+sel.fit(head_df / head_df.mean())
+
+# Create a boolean mask
+mask = sel.get_support()
+
+# Apply the mask to create a reduced dataframe
+reduced_df = head_df.loc[:, mask]
+
+print("Dimensionality reduced from {} to {}.".format(head_df.shape[1], reduced_df.shape[1]))
+
+
+"""
+OUTPUT:
+
+Dimensionality reduced from 6 to 4.
+
+
+
+"""
