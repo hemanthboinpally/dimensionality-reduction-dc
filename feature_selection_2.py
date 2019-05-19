5d6579d259e5b0053726ab267453a9a682c912fa
hemanthboinpally <hemanth.boinpally@gmail.com>
2019-05-18 19:06:08 -0700

    Manual Recursive Feature Elimination

--- feature_selection_2.py
+++ feature_selection_2.py
@@ -33,5 +33,40 @@ print(dict(zip(X.columns, abs(lr.coef_[0]).round(2))))
 79.6% accuracy on test set.
 {'bmi': 0.38, 'insulin': 0.19, 'glucose': 1.23, 'diastolic': 0.03, 'family': 0.34, 'age': 0.34, 'triceps': 0.24, 'pregnant': 0.04}
 
+Great! We get almost 80% accuracy on the test set. Take a look at the differences in model coefficients for the different features.
 """
 
+
+
+"""
+2) Manual Recursive Feature Elimination 
+
+Now that we've created a diabetes classifier, let's see if we can reduce the number of features without hurting the 
+model accuracy too much.
+
+On the second line of code the features are selected from the original dataframe. Adjust this selection.
+
+A StandardScaler() instance has been predefined as scaler and a LogisticRegression() one as lr.
+
+All necessary functions and packages have been pre-loaded too.
+
+
+"""
+
+# Only keep the feature with the highest coefficient
+X = diabetes_df[['glucose', 'triceps', 'bmi', 'family', 'age']]
+
+# Performs a 25-75% train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+# Scales features and fits the logistic regression model to the data
+lr.fit(scaler.fit_transform(X_train), y_train)
+
+# Calculates the accuracy on the test set and prints coefficients
+acc = accuracy_score(y_test, lr.predict(scaler.transform(X_test)))
+print("{0:.1%} accuracy on test set.".format(acc))
+print(dict(zip(X.columns, abs(lr.coef_[0]).round(2))))
+
+"""
+Removed the features, one after another based on the lowest coefficient or the feature importance. 
+"""
