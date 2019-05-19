ab842012b55e064a8e82bc77c150de2fa4c0c212
hemanthboinpally <hemanth.boinpally@gmail.com>
2019-05-18 19:11:50 -0700

    Automatic Recursive Feature Elimination

--- feature_selection_2.py
+++ feature_selection_2.py
@@ -70,3 +70,47 @@ print(dict(zip(X.columns, abs(lr.coef_[0]).round(2))))
 """
 Removed the features, one after another based on the lowest coefficient or the feature importance. 
 """
+
+######################
+
+
+"""
+3) Automatic Recursive Feature Elimination
+Now let's automate this recursive process. Wrap a Recursive Feature Eliminator (RFE) 
+around our logistic regression estimator and pass it the desired number of features.
+
+All the necessary functions and packages have been pre-loaded and the features have been scaled for you.
+
+"""
+from sklearn.feature_selection import RFE
+
+# Create the RFE with a LogisticRegression estimator and 3 features to select
+rfe = RFE(estimator=LogisticRegression(), n_features_to_select=3, verbose=1)
+
+# Fits the eliminator to the data
+rfe.fit(X_train, y_train)
+
+# Print the features and their ranking (high = dropped early on)
+print(dict(zip(X.columns, rfe.ranking_)))
+
+# Print the features that are not eliminated
+print(X.columns[rfe.support_])
+
+# Calculates the test set accuracy
+acc = accuracy_score(y_test, rfe.predict(X_test))
+print("{0:.1%} accuracy on test set.".format(acc))
+
+
+"""
+Fitting estimator with 8 features.
+Fitting estimator with 7 features.
+Fitting estimator with 6 features.
+Fitting estimator with 5 features.
+Fitting estimator with 4 features.
+
+{'bmi': 1, 'insulin': 4, 'glucose': 1, 'diastolic': 6, 'family': 2, 'age': 1, 'triceps': 3, 'pregnant': 5}
+
+Index(['glucose', 'bmi', 'age'], dtype='object')
+
+80.6% accuracy on test set.
+"""
