f14e286996c278ca30b1c9cd0d88df440c6d22bd
hemanthboinpally <hemanth.boinpally@gmail.com>
2019-05-18 18:15:25 -0700

    Pairwise Correlation Plot

--- feature_selection_1.py
+++ feature_selection_1.py
@@ -212,7 +212,49 @@ tlt           0.000000
 (131, 21)
 (131, 19)
 
+"""
+
+
+
+###########################
+
+
+"""
+6) Pairwise Correlation
+
+Features that are related 
+.corr()
 
 
 """
 
+# Create the correlation matrix
+corr = ansur_df.corr()
+
+# Draw the heatmap
+sns.heatmap(corr,  cmap=cmap, center=0, linewidths=1, annot=True, fmt=".2f")
+plt.show()
+
+
+
+#Mask the upper triangle.
+
+# Create the correlation matrix
+corr = ansur_df.corr()
+
+# Generate a mask for the upper triangle
+mask = np.triu(np.ones_like(corr, dtype=bool))
+
+# Add the mask to the heatmap
+sns.heatmap(corr, mask=mask, cmap=cmap, center=0, linewidths=1, annot=True, fmt=".2f")
+plt.show()
+
+"""
+         Elbow rest height  Wrist circumference  Ankle circumference  Buttock height  Crotch height
+Elbow rest height             1.000000             0.294753             0.301963       -0.007013      -0.026090
+Wrist circumference           0.294753             1.000000             0.702178        0.576679       0.606582
+Ankle circumference           0.301963             0.702178             1.000000        0.367548       0.386502
+Buttock height               -0.007013             0.576679             0.367548        1.000000       0.929411
+Crotch height                -0.026090             0.606582             0.386502        0.929411       1.000000
+
+"""
