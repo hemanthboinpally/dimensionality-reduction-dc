002c3b60d819b695c2f12c80bb4ea1b4a81bae64
hemanthboinpally <hemanth.boinpally@gmail.com>
2019-05-24 07:19:26 -0700

    Lasso Regression - Adjust alpha

--- feature_selection_2.py
+++ feature_selection_2.py
@@ -304,3 +304,37 @@ Good! We can predict almost 85% of the variance in the BMI value using just 9 ou
 The R^2 could be higher though.
 
 """
+
+
+#########################
+
+"""
+8) Adjusting the regularization strength
+Your current Lasso model has an R2 score of 84.7%. When a model applies overly powerful regularization 
+it can suffer from high bias, hurting its predictive power.
+
+Let's improve the balance between predictive power and model simplicity by tweaking the alpha parameter.
+
+Find the highest value for alpha that keeps the R2 value above 98% from the options: 1, 0.5, 0.1, and 0.01.
+
+"""
+# Find the highest alpha value with R-squared above 98%
+la = Lasso(alpha=0.1, random_state=0)
+
+# Fits the model and calculates performance stats
+la.fit(X_train_std, y_train)
+r_squared = la.score(X_test_std, y_test)
+n_ignored_features = sum(la.coef_ == 0)
+
+# Print peformance stats
+print("The model can predict {0:.1%} of the variance in the test set.".format(r_squared))
+print("{} out of {} features were ignored.".format(n_ignored_features, len(la.coef_)))
+
+"""
+OUTPUT:
+    The model can predict 98.3% of the variance in the test set.
+    64 out of 91 features were ignored.
+
+Wow! We this more appropriate regularization strength we can predict 98% of the variance in the 
+BMI value while ignoring 2/3 of the features.
+"""
