1660f421994e97963469be1c120117642aa64ec8
hemanthboinpally <hemanth.boinpally@gmail.com>
2019-05-24 07:38:47 -0700

    Combining Models for Feature Selection

--- feature_selection_2.py
+++ feature_selection_2.py
@@ -374,3 +374,85 @@ print('{} features out of {} selected'.format(sum(lcv_mask), len(lcv_mask)))
 We got a decent R squared and removed 5 features. We'll save the lcv_mask for later on.
 
 """
+
+###################################
+
+"""
+10) Ensemble models for extra votes
+The LassoCV() model selected 27 out of 32 features. Not bad, but not a spectacular dimensionality reduction either.
+ Let's use two more models to select the 10 features they consider most important using the Recursive Feature Eliminator
+ (RFE).
+
+The standardized training and test data has been pre-loaded for you as X_train, X_test, y_train, and y_test.
+
+"""
+
+from sklearn.feature_selection import RFE
+from sklearn.ensemble import GradientBoostingRegressor
+
+# Select 10 features with RFE on a GradientBoostingRegressor, drop 3 features on each step
+rfe_gb = RFE(estimator=GradientBoostingRegressor(),
+             n_features_to_select=10, step=3, verbose=1)
+rfe_gb.fit(X_train, y_train)
+
+
+# Calculate the R squared on the test set
+r_squared = rfe_gb.score(X_test,y_test)
+print('The model can explain {0:.1%} of the variance in the test set'.format(r_squared))
+
+# Assign the support array to gb_mask
+gb_mask = rfe_gb.support_
+
+"""
+Fitting estimator with 32 features.
+Fitting estimator with 29 features.
+Fitting estimator with 26 features.
+Fitting estimator with 23 features.
+Fitting estimator with 20 features.
+Fitting estimator with 17 features.
+Fitting estimator with 14 features.
+Fitting estimator with 11 features.
+Out[1]: 
+RFE(estimator=GradientBoostingRegressor(alpha=0.9, criterion='friedman_mse', init=None,
+             learning_rate=0.1, loss='ls', max_depth=3, max_features=None,
+             max_leaf_nodes=None, min_impurity_decrease=0.0,
+             min_impurity_split=None, min_samples_leaf=1,
+             min_sampl...=None, subsample=1.0, tol=0.0001,
+             validation_fraction=0.1, verbose=0, warm_start=False),
+  n_features_to_select=10, step=3, verbose=1)
+  
+  The model can explain 85.6% of the variance in the test set
+"""
+
+
+from sklearn.feature_selection import RFE
+from sklearn.ensemble import RandomForestRegressor
+
+# Select 10 features with RFE on a RandomForestRegressor, drop 3 features on each step
+rfe_rf = RFE(estimator=RandomForestRegressor(),
+             n_features_to_select=10, step=3, verbose=1)
+rfe_rf.fit(X_train, y_train)
+
+# Calculate the R squared on the test set
+r_squared = rfe_rf.score(X_test, y_test)
+print('The model can explain {0:.1%} of the variance in the test set'.format(r_squared))
+
+# Assign the support array to gb_mask
+rf_mask = rfe_rf.support_
+
+
+"""
+    Fitting estimator with 32 features.
+    Fitting estimator with 29 features.
+    Fitting estimator with 26 features.
+    Fitting estimator with 23 features.
+    Fitting estimator with 20 features.
+    Fitting estimator with 17 features.
+    Fitting estimator with 14 features.
+    Fitting estimator with 11 features.
+    The model can explain 84.0% of the variance in the test set
+    
+Inluding the Lasso linear model from the previous exercise, we now have the votes from 3 models on which 
+features are important.
+
+"""
