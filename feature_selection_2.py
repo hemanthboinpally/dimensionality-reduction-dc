c5ebeb35c77776ee4cd29bd7e876d4f23eb80873
hemanthboinpally <hemanth.boinpally@gmail.com>
2019-05-24 07:05:37 -0700

    Lasso Regression

--- feature_selection_2.py
+++ feature_selection_2.py
@@ -231,5 +231,34 @@ exercise one of the selected features is different.
 ################
 
 """
+6) Creating a LASSO regressor
 
+You'll be working on the numeric ANSUR body measurements dataset to predict a persons Body Mass Index (BMI) using the pre-imported Lasso() regressor. BMI is a metric derived from body height and weight but those two features have been removed from the dataset to give the model a challenge.
+
+You'll standardize the data first using the StandardScaler() that has been instantiated for you as scaler to make sure all coefficients face a comparable regularizing force trying to bring them down.
+
+All necessary functions and classes plus the input datasets X and y have been pre-loaded.
+
+"""
+
+# Set the test size to 30% to get a 70-30% train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3, random_state=0)
+
+# Fit the scaler on the training features and transform these in one go
+X_train_std = scaler.fit_transform(X_train)
+
+# Create the Lasso model
+la = Lasso()
+
+# Fit it to the standardized training data
+la.fit(X_train_std,y_train)
+
+
+"""
+Out[3]: 
+Lasso(alpha=1.0, copy_X=True, fit_intercept=True, max_iter=1000,
+   normalize=False, positive=False, precompute=False, random_state=None,
+   selection='cyclic', tol=0.0001, warm_start=False)
+   
+   
 """
