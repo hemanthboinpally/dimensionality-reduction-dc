fedade8d83072eb9de76bd23273e5c1e4fde66b3
hemanthboinpally <hemanth.boinpally@gmail.com>
2019-05-24 07:46:28 -0700

    Combining 3 Selectors to get the right features

--- feature_selection_2.py
+++ feature_selection_2.py
@@ -456,3 +456,41 @@ Inluding the Lasso linear model from the previous exercise, we now have the vote
 features are important.
 
 """
+
+#######################################
+
+
+"""
+11) Combining 3 feature selectors
+We'll combine the votes of the 3 models you built in the previous exercises, 
+to decide which features are important into a meta mask. We'll then use this mask to reduce dimensionality 
+and see how a simple linear regressor performs on the reduced dataset.
+
+The per model votes have been pre-loaded as lcv_mask, rf_mask, and gb_mask and the feature and target datasets as X and y.
+"""
+
+# Sum the votes of the three models
+votes = np.sum([lcv_mask, rf_mask, gb_mask], axis=0)
+
+# Create a mask for features selected by all 3 models
+meta_mask = votes >= 3
+
+# Apply the dimensionality reduction on X
+X_reduced = X.loc[:, meta_mask]
+
+# Plug the reduced dataset into a linear regression pipeline
+X_train, X_test, y_train, y_test = train_test_split(X_reduced, y, test_size=0.3, random_state=0)
+lm.fit(scaler.fit_transform(X_train), y_train)
+r_squared = lm.score(scaler.transform(X_test), y_test)
+
+print('The model can explain {0:.1%} of the variance in the test set using {1:} features.'.format(r_squared, len(lm.coef_)))
+
+"""
+
+<script.py> output:
+    The model can explain 86.8% of the variance in the test set using 7 features.
+    
+Using the votes from 3 models you were able to select just 7 features that allowed a simple linear model to get a 
+high accuracy!
+
+"""
