33c2e2b88ae7c736da606d41a7687a11d63e5c4e
hemanthboinpally <hemanth.boinpally@gmail.com>
2019-05-24 07:29:19 -0700

    Lasso Regression - Adjust alpha using Regressor

--- feature_selection_2.py
+++ feature_selection_2.py
@@ -338,3 +338,39 @@ OUTPUT:
 Wow! We this more appropriate regularization strength we can predict 98% of the variance in the 
 BMI value while ignoring 2/3 of the features.
 """
+##########################################################################################
+
+"""
+9) Creating a LassoCV regressor
+You'll be predicting biceps circumference on a subsample of the male ANSUR dataset using the LassoCV() regressor 
+that automatically tunes the regularization strength (alpha value) using Cross-Validation.
+
+The standardized training and test data has been pre-loaded for you as X_train, X_test, y_train, and y_test
+
+"""
+
+from sklearn.linear_model import LassoCV
+
+# Create and fit the LassoCV model on the training set
+lcv = LassoCV()
+lcv.fit(X_train,y_train)
+print('Optimal alpha = {0:.3f}'.format(lcv.alpha_))
+
+# Calculate R squared on the test set
+r_squared = lcv.score(X_test,y_test)
+print('The model explains {0:.1%} of the test set variance'.format(r_squared))
+
+# Create a mask for coefficients not equal to zero
+lcv_mask = lcv.coef_ !=0
+print('{} features out of {} selected'.format(sum(lcv_mask), len(lcv_mask)))
+
+
+"""
+<script.py> output:
+    Optimal alpha = 0.089
+    The model explains 88.2% of the test set variance
+    26 features out of 32 selected
+
+We got a decent R squared and removed 5 features. We'll save the lcv_mask for later on.
+
+"""
