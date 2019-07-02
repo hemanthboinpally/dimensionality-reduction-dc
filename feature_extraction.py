906210ebfb714c020545b0629a3e3bc7af9d6c2b
hemanthboinpally <hemanth.boinpally@gmail.com>
2019-07-01 23:08:16 -0700

    PCA Explained Variance

--- feature_extraction.py
+++ feature_extraction.py
@@ -115,4 +115,36 @@ pca.fit(ansur_std)
 """
 Great! You've fitted PCA on our 13 feature datasample. Now let's see how the components explain the variance.
 
+
+"""
+
+
+"""
+5) PCA explained variance
+You'll be inspecting the variance explained by the different principal components of the pca instance 
+you created in the previous exercise.
+
+"""
+
+# Inspect the explained variance ratio per component
+print(pca.explained_variance_)
+
+# Print the cumulative sum of the explained variance ratio
+print(pca.explained_variance_ratio_.cumsum())
+
+"""
+
+Explained Variance: 
+
+[0.61449404 0.19893965 0.06803095 0.03770499 0.03031502 0.0171759
+ 0.01072762 0.00656681 0.00634743 0.00436015 0.0026586  0.00202617
+ 0.00065268]
+
+Cumulative Sum: 
+
+[0.61449404 0.81343368 0.88146463 0.91916962 0.94948464 0.96666054
+ 0.97738816 0.98395496 0.99030239 0.99466254 0.99732115 0.99934732
+ 1.        ]
+ 
+ 
 """
